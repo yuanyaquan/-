d4fbbf4513be536689a8aa68eae413f255dfe84d
yuanyaquan <yuanyaquan@cnpc.com.cn>
2019-10-22 09:45:08 +0800

    初始化项目

---
+++ src/api/ajax.js
@@ -0,0 +1,33 @@
+/*
+ajax请求模块
+返回值：promise对象
+*/
+import axios from 'axios'
+export default function ajax(url = '', data = {}, type = 'GET') {
+    // resolve,reject是两个函数，函数作为参数传入函数中被称为高阶函数
+    return new Promise(function (resolve, reject) {
+        let promise
+        if (type === 'GET') { 
+            // 准备 url query 参数数据 
+            let dataStr = '' 
+            //数据拼接字符串 
+            Object.keys(data).forEach(key => {
+                dataStr += key + '=' + data[key] + '&' 
+            })
+            if (dataStr !== '') {
+                dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+                url = url + '?' + dataStr 
+            }
+            // 发送 get 请求 
+            promise = axios.get(url) 
+        } else { 
+            // 发送 post 请求 
+            promise = axios.post(url, data) 
+        }
+        promise.then(response => {
+            resolve(response.data) 
+        }).catch(error => {
+            reject(error) 
+        }) 
+    })
+}
